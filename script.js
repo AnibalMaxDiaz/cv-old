ecd98cc054cab309faf8ea187b08a13393497244
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2024-09-19 17:26:07 -0500

    change style of the site

--- script.js
+++ script.js
@@ -1,76 +1,11 @@
-// alert("Hola este es mi Javascript");
-let minombre="Aníbal";
-console.log(minombre);
-// let listas=document.getElementsByTagName("li");
-// console.log(listas);
-let contenidoTitulo="Aníbal Max";
-let titulo=document.querySelector(".titulo")
-titulo.innerHTML = contenidoTitulo;
+// Next Project
 
-let textoTitulo=titulo.innerText;
-if (textoTitulo=="minombre"){
-    titulo.innerHTML = "otro";
+document.getElementById("card4").style.display = "none";
+document.getElementById("skills").style.display = "none";
+
+
+let email = document.getElementById("email").innerText;
+let button = document.getElementById("copy-button")
+function copy(){
+  navigator.clipboard.writeText(email)
 }
-else{
-    console.log("Este es un aviso de prueba = No se cumple")
-}
-
-
-let nombre1="Aníbal Max";
-let ciudad="Bogotá Colómbia";
-let perfil="profesional";
-
-let parrafo = document.querySelector(".parrafo-2");
-
-function cambiarTexto(nombre1, ciudad, perfil){
-let contenido=`Me llamo ${nombre1}, nací en ${ciudad}, soy ingeniero de sistemas y
-tecnologo en telecomunicaciones, me gusta la tecnología el deporte
-y salir a pasear con mi familia. Me encanta la
-programación, mi ojetivo es poder mejorar mi perfil ${perfil} explotando mi creatividad y capacidades en pro de los demas.
-<b>"En las secciones mas adelante podrás ver mis logros y proyectos".</b>`;
-
-return contenido
-};
-parrafo.innerText = cambiarTexto(nombre1, ciudad, perfil);
-
-let menu_responsive = document.querySelector(".checkbtn");
-menu_responsive.onclick = function () {
-  navBar = document.querySelector(".navbar");
-  navBar.classList.toggle("active");
-};
-
-console.log("navBar por menu???")
-
-// Formulario-------------------------------------------
-
-const form = document.getElementById("form");
-const nombre = document.getElementById("nombre");
-const parrafo = document.getElementById("alertas");
-
-function validarFormulario() {
-  let warnings = "";
-  let valido = true;
-  parrafo.innerHTML = "";
-
-  if (nombre.value.length < 4) {
-    warnings += `El nombre debe contener más de 4 carcateres`;
-    valido = false;
-  }
-
-  if (!valido) {
-    parrafo.innerHTML = warnings;
-  } else {
-    parrafo.innerHTML = "Enviado";
-  }
-  return valido;
-}
-
-form.addEventListener("submit", (e) => {
-  if (validarFormulario()) {
-    // Si la validación es exitosa, puedes enviar el formulario
-    formulario.submit();
-  } else {
-    e.preventDefault(); // Evita que el formulario se envíe automáticamente
-  }
-
-});
